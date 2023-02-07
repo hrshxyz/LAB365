0d17d988ade9d0e12ebd872dc41b64a863224521
Henrique Ribeiro Souto <hrshxyz@gmail.com>
2023-02-07 18:41:31 -0300

    Ajuste da msg de confirmação e adicionado dois arquivos da primeira aula da semana 2.

---
+++ Modulo 001/Semana 2/Aula 001/functions.js
@@ -0,0 +1,25 @@
+console.log("Hello ");
+
+for (i = 0; i < 10; i++) {
+    console.log('World');
+}
+
+const alunos = ['1', '2', '3'];
+alunos.forEach((aluno) => {
+    console.log(aluno);
+});
+
+let somaNumeroes = 0
+const numeros = [1, 2, 3, 4, 5];
+numeros.forEach((numero) => { 
+    somaNumeroes += numero; 
+});
+const media = somaNumeroes / numeros.length;
+console.log(media);
+
+function multiplicationTable() {
+    let number = document.getElementById('inputNumber').value;
+    for ( index=1 ; index <=10 ; index++ ) {
+        console.log(number*index)
+    }
+}
