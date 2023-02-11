93a227deea2753b1f0d8c77e7ddc8c09c9f27c49
Henrique Ribeiro Souto <hrshxyz@gmail.com>
2023-02-11 20:49:16 -0300

    removi tr a mais, e alterado cores no css

--- Modulo 001/Semana 2/Exercicio 2/Ex 7 - PadariaTech/js/functions.js
+++ Modulo 001/Semana 2/Exercicio 2/Ex 7 - PadariaTech/js/functions.js
@@ -8,10 +8,9 @@ function calculatePrice() {
     let price = document.getElementById('price').value;
     for ( index=1 ; index <=50 ; index++ ) {
         let calc = parseFloat(price*index).toFixed(2);
-        tabela += '<tr>';
         tabela += '<tr><td align="center">' + index + '</td>';
         tabela += '<td align="center"> R$ ' + calc + '</td> </tr>';
-        tabela += '</tr>';
     }
+    tabela += '</table>';
     listp.innerHTML = tabela;
 };
