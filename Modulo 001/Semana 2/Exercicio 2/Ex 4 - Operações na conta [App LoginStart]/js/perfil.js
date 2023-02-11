4e1366a290b8ff05f8be77922c8791ceae07a88f
Henrique Ribeiro Souto <hrshxyz@gmail.com>
2023-02-11 16:15:39 -0300

    ajuste no js, para adicionar um padrão no localstorage sem apagar as novas informações.

--- Modulo 001/Semana 2/Exercicio 2/Ex 4 - Operações na conta [App LoginStart]/js/perfil.js
+++ Modulo 001/Semana 2/Exercicio 2/Ex 4 - Operações na conta [App LoginStart]/js/perfil.js
@@ -1,24 +1,57 @@
-const {nameProfile, emailProfile, imageProfile} = {
-    nameProfile     : 'Henrique',
-    emailProfile    : 'henrique@esudante.senai',
-    imageProfile    : 'puff.png'
+const {defaultNameProfile, defaultEmailProfile, defaultImageProfile} = {
+    defaultNameProfile     : 'Henrique',
+    defaultEmailProfile    : 'henrique@esudante.senai',
+    defaultImageProfile    : 'https://picsum.photos/200/300/?blur=2'
 };
+var nameProfile = localStorage.getItem('nameProfile');
+if (nameProfile == null) { localStorage.setItem('nameProfile', defaultNameProfile); };
+
+var emailProfile = localStorage.getItem('emailProfile');
+if (emailProfile == null) { localStorage.setItem('emailProfile', defaultEmailProfile); };
+
+var imageProfile = localStorage.getItem('imageProfile');
+if (imageProfile == null) { localStorage.setItem('imageProfile', defaultImageProfile); };
 
 function setImage() {
     var image = document.createElement('img');
-    image.src = `./images/${imageProfile}`
+    image.src = imageProfile
     document.getElementById('imageProfile').appendChild(image);
 };
 
-nameProfile != ''
-    ? document.getElementById('nameProfile').innerHTML = `Logado como <strong>${nameProfile}</strong>`
-    : document.getElementById('nameProfile').innerHTML = `Logado como <strong>Usuário sem Identificação</strong>!`
+function changeUserName() {
+    const changeNameProfile = document.getElementById('name').value;
+    console.log(changeNameProfile)
+    localStorage.setItem('nameProfile', changeNameProfile);
+    window.location.reload(true);
+}
 
-emailProfile != ''
-    ? document.getElementById('emailProfile').innerHTML = `E-mail: <strong>${emailProfile}</strong>`
-    : document.getElementById('emailProfile').innerHTML = `E-mail <strong>Usuário sem e-mail</strong>!`
+function changeEmailProfile() {
+    const changeEmailProfile = document.getElementById('email').value;
+    localStorage.setItem('emailProfile', changeEmailProfile);
+    window.location.reload(true);
+}
 
-imageProfile != ''
-    ? setImage()
-    : document.getElementById('imageProfile').innerHTML = 'Perfil Sem Imagem!'
+function changeImageProfile() {
+    const changeImageProfile = document.getElementById('image').value;
+    console.log(changeImageProfile)
+    localStorage.setItem('imageProfile', changeImageProfile);
+    window.location.reload(true);
+}
 
+nameProfile     = localStorage.getItem('nameProfile'),
+emailProfile    = localStorage.getItem('emailProfile'),
+imageProfile    = localStorage.getItem('imageProfile'),
+
+window.onload = (event) => {
+    nameProfile != ''
+        ? document.getElementById('nameProfile').innerHTML = `Logado como <strong>${nameProfile}</strong>`
+        : document.getElementById('nameProfile').innerHTML = `Logado como <strong>Usuário sem Identificação</strong>!`
+
+    emailProfile != ''
+        ? document.getElementById('emailProfile').innerHTML = `E-mail: <strong>${emailProfile}</strong>`
+        : document.getElementById('emailProfile').innerHTML = `E-mail <strong>Usuário sem e-mail</strong>!`
+
+    imageProfile != ''
+        ? setImage()
+        : document.getElementById('imageProfile').innerHTML = 'Perfil Sem Imagem!'
+}
