d8cef7e5f7f79d5465be102fa3dd72e9343e1fb4
Henrique Ribeiro Souto <hrshxyz@gmail.com>
2023-02-12 15:10:53 -0300

    commit de funções do app.

--- Modulo 001/Semana 2/Exercicio 2/Ex 8 - PadariaTechPlus/js/functions.js
+++ Modulo 001/Semana 2/Exercicio 2/Ex 8 - PadariaTechPlus/js/functions.js
@@ -1,27 +1,97 @@
+window.onload = function() {
+    document.getElementById('afterTotal').style.display = 'none';
+    document.getElementById('confirm').style.display = 'none';
+}
+
+const prices = [];
+let totalPayable = 0;
+
 function calculatePrice(price) {
-    let listp = document.getElementById('result');
-    let tabela = '<table>';
+        document.getElementById('afterTotal').style.display = 'none';
+        let listp = document.getElementById('result');
+        inputPrice      = document.getElementById('price').value;
+        prices.push(inputPrice);
+        console.log(prices);
 
-    tabela += '<tr><th align="center"> Quantidade de Pães</th>';
-    tabela += '<th align="center"> Valores</th> </tr>';
+        let tabela = '<table>';
+        tabela += '<tr><th align="center"> Código dos Produtos</th>';
+        tabela += '<th align="center"> Valores</th> </tr>';
+        
+        prices.forEach( (price, index) => {
+                let calc = parseFloat(price).toFixed(2);
+                index++
+                tabela += '<tr><td align="center">' + index + '</td>';
+                tabela += '<td align="center"> R$ ' + calc + '</td> </tr>';        
+            }
+        );
 
-    for ( index=1 ; index <=50 ; index++ ) {
-        let calc = parseFloat(price*index).toFixed(2);
-        tabela += '<tr><td align="center">' + index + '</td>';
-        tabela += '<td align="center"> R$ ' + calc + '</td> </tr>';
-    }
-    tabela += '</table>';
-    listp.innerHTML = tabela;
+        tabela += '</table>';
+        listp.innerHTML = tabela;
 };
 
-function alert() {
-    var listp = document.getElementById('result');
-    listp.innerHTML = 'Entre com um valor acima de zero!'
+function endProducts() {
+    let listp = document.getElementById('total');
+    let tabela = '<table>';
+    tabela += '<tr><th align="center"> Quantidade de Produtos</th>';
+    tabela += '<th align="center"> Total </th> </tr>';
+    
+    let total = 0;
+    let totalProducts = 0;
+    prices.forEach( (price, index) => {
+            let calc = parseFloat(price).toFixed(2);
+            total += parseFloat(calc);
+            totalProducts = ++index;
+        }
+    );
+    total = parseFloat(total).toFixed(2);
+    totalPayable = total;
+
+    tabela += '<tr><td align="center" Total>' + totalProducts +'</td>';
+    tabela += '<td align="center"> R$ ' + total + '</td> </tr>';       
+    tabela += '</table>';
+    listp.innerHTML = tabela;
+
+    document.getElementById('afterTotal').style.display = 'block';
 }
 
 function checkValue() {
     var price = document.getElementById('price').value;
     price > 0
         ? calculatePrice(price)
-        : alert()
+        : endProducts()
 }
+
+function exchangeMsg(payment) {
+    const exchange = document.getElementById('exchange');
+    payment = parseFloat(payment).toFixed(2);
+    console.log(payment);
+    let tabela = '<table>';
+    tabela += '<tr><th align="center"> Troco </th></tr>';
+    tabela += '<tr><td align="center">' + payment +'</td>';
+    tabela += '</table>';
+    exchange.innerHTML = tabela;
+    document.getElementById('confirm').style.display = 'block';
+}
+
+function confirm() {
+    window.location.reload(true)
+}
+
+function payment() {
+    let money = document.getElementById('money').value;
+    const exchange = document.getElementById('exchange');
+
+    money = parseFloat(money).toFixed(2);
+    totalPayable = parseFloat(totalPayable).toFixed(2);
+
+    const payment = money - totalPayable;
+    console.log(money);
+    console.log(totalPayable)
+    console.log(payment)
+    console.log(Math.sign(payment))
+    
+    Math.sign(payment) != -1
+        ? ( exchangeMsg(payment), exchange.removeAttribute("class") )
+        : ( exchange.innerHTML = ('Valor menor que o total.'), exchange.setAttribute("class", "alert") )
+}
+
