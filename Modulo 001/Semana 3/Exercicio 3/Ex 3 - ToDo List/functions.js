a5895a547acb6161143833dd37e992c53ce62bf7
Henrique Ribeiro Souto <hrshxyz@gmail.com>
2023-02-18 23:25:51 -0300

    adicionado add e remove task

--- Modulo 001/Semana 3/Exercicio 3/Ex 3 - ToDo List/functions.js
+++ Modulo 001/Semana 3/Exercicio 3/Ex 3 - ToDo List/functions.js
@@ -9,6 +9,15 @@ const data = [
     },
 ];
 
+window.onload = function() {
+    document.getElementById('changeTaskList').style.display = 'none';
+    document.getElementById('changetaskDescriptionList').style.display = 'none';
+    document.getElementById('addTaskList').style.display = 'none';
+    document.getElementById('removeTaskList').style.display = 'none';
+    document.getElementById('searchTask').style.display = 'none';
+    
+}
+
 function mountTasklist() {
     let allTaskList = document.getElementById("allTaskList");
     let ul = document.getElementById("task");
@@ -17,7 +26,7 @@ function mountTasklist() {
     ul.innerHTML ='';
     data.forEach((task) => {
         const li = document.createElement("li");
-        li.innerHTML = (`Nome da Task: ${task.task} => Descrição da Task: ${task.taskDescription}`);
+        li.innerHTML = (`<b>Nome da Task:</b> ${task.task} => <b>Descrição da Task:</b> ${task.taskDescription}`);
         ul.appendChild(li);
     });
 }
@@ -25,8 +34,9 @@ mountTasklist()
 setInterval(mountTasklist,1000)
 
 function printUser(task, taskDescription) {
+    document.getElementById('printSearch').style.display = 'block';
     const ul = document.getElementById("task");
-    const p = document.getElementById("user");
+    const p = document.getElementById("printSearch");
     p.innerHTML = (`${task} - ${taskDescription}`);
     console.log(data)
 };
@@ -45,12 +55,39 @@ const msgUser = "Tarefa não encontrada!";
 let indexTask = 0;
 
 function searchTask() {
+    document.getElementById('changeTaskList').style.display = 'none';
+    document.getElementById('changetaskDescriptionList').style.display = 'none';
+    document.getElementById('printSearch').style.display = 'none';
+    document.getElementById('removeTaskList').style.display = 'none';
+    document.getElementById('searchTask').style.display = 'block';
+    document.getElementById('addTaskList').style.display = 'none';
+    document.getElementById('changeTask').value = '';
+    document.getElementById('taskDescription').value = '';
+
     const task = document.getElementById('searchTask').value;
     let searchTask = data.reduce((atual, valor, indice)=>{
         if (valor.task == task) { 
+            document.getElementById('changeTaskList').style.display = 'block';
+            document.getElementById('changetaskDescriptionList').style.display = 'block';
+            document.getElementById('removeTaskList').style.display = 'block';
             indexTask = indice;
             printUser(valor.task, valor.taskDescription)
         }
 
     },0)
 };
+
+function addTask() {
+    const task = document.getElementById('addTask').value;
+    const taskDescription = document.getElementById('addTaskDescription').value;
+    document.getElementById('addTaskList').style.display = 'block';
+    if (task != '' && task != undefined && task != null){
+        data.push({ taskDescription  : String(taskDescription), 
+                    task             : String(task)
+                });
+    }
+}
+
+function removeTask() {
+    data.splice(indexTask, 1)
+}
