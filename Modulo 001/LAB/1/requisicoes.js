8700a1f190f6b4a199dacd3115065bda324097ad
Henrique Ribeiro Souto <hrshxyz@gmail.com>
2023-02-22 20:43:20 -0300

    add  labs

---
+++ Modulo 001/LAB/1/requisicoes.js
@@ -0,0 +1,15 @@
+/* const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://reqres.in/api/users");
+xhr.responseType = "json"
+xhr.onload = () => {
+    console.log(xhr.response.data)
+};
+
+xhr.send(); */
+
+/* const infos = { method: 'GET' };
+fetch("https://reqres.in/api/users", infos)
+.then(response => response.json())
+.then(dados => console.log(dados.data)); */
+
+
