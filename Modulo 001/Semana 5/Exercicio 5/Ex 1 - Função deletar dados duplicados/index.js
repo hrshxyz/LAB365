be288f4357eeda98602205fa32545c1780bf8d93
Henrique Ribeiro Souto <hrshxyz@gmail.com>
2023-03-02 19:49:07 -0300

    ajuste de nomes

--- Modulo 001/Semana 5/Exercicio 5/Ex 1 - Função deletar dados duplicados/index.js
+++ Modulo 001/Semana 5/Exercicio 5/Ex 1 - Função deletar dados duplicados/index.js
@@ -1,11 +1,11 @@
 const lista =  ['Pedro', 'José', 'Aderbal', 'Danilo', 'Luisa', 'Vitoria', 'Luis', 'Danilo', 'José', 'hrs', 'hrs'];
 let duplicado = [];
 
-const filteredArray = lista.filter(function(elemento , posicao){
+const listaDuplicada = lista.filter(function(elemento , posicao){
     if (lista.indexOf(elemento) != posicao ) {
         duplicado.push(lista[posicao]);
     }
     return lista.indexOf(elemento) == posicao;
 }) 
 
-console.log(duplicado,filteredArray);
+console.log(duplicado,listaDuplicada);
