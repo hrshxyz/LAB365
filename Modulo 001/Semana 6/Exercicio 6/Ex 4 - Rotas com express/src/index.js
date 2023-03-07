14283a45ce600b01c1b78ed4ee7c4d8312154cbd
Henrique Ribeiro Souto <hrshxyz@gmail.com>
2023-03-06 22:07:55 -0300

    commit inicial, falta validade quando não envia o nome

---
+++ Modulo 001/Semana 6/Exercicio 6/Ex 4 - Rotas com express/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const application = express();
+application.use(express.json());
+application.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+})
+
+/* application.get('/', (requisicao, resposta) => {
+    resposta.send("Ex 4 - Rotas com express! Para enviar um nome entre com a rota /enviar utliza um app para fazer post.")
+}) */
+
+application.post('/enviar/:nome', (requisicao, resposta) => {
+    const { nome } = requisicao.params;
+    if (!nome) return resposta
+    .status(400)
+    .json({ "erro": "Campo nome é obrigatório" })
+   
+    console.log(`Rota(/enviar) da API criada pelo(a): ${nome}`);
+    resposta.send(`Nome enviado! ${nome}`)
+})
