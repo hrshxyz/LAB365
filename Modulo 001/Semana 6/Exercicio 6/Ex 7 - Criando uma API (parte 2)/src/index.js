0581f8d5a2d504a6ab38bcb330d1a2a1b6383536
Henrique Ribeiro Souto <hrshxyz@gmail.com>
2023-03-11 11:14:38 -0300

    Commit Ex 7 - Criando uma API (parte 2)

---
+++ Modulo 001/Semana 6/Exercicio 6/Ex 7 - Criando uma API (parte 2)/src/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const port = 3333;
+
+app.use(express.json())
+
+let users = [];
+
+app.get('/users', (req, res) => {
+    res.send(users);
+});
+
+app.post('/user', (req, res) => {
+    const { nome, idade, cargo, senha } = req.body;
+
+    if (Object.keys(req.body).length < 4) {
+        res.status(406).json({ "mensagem": "Está faltando dados para concluir a operação" });
+        return;
+    };
+
+    if (idade < 21) {
+        res.status(401).json({ "mensagem": "Usuário não possui idade suficiente" });
+        return;
+    };
+
+    let idPost = users.length + 1;
+    users.push({ "id": idPost, "nome": nome, "idade": idade, "cargo": cargo, "senha": senha });
+    res.status(200).json({ "mensagem": "Criado com sucesso" });
+});
+
+app.delete('/user', (req, res) => {
+    let id = req.query.id * 1;
+
+    if (!id) {
+        res.status(406).json({ "mensagem": "Está faltando dados para concluir a operação" });
+        return;
+    };
+
+    let idDelete = users.find(idDel => idDel.id === id);
+    let index    = users.indexOf(idDelete);
+
+    if (idDelete) {
+        users.splice(index, 1);
+        res.status(200).send("ID deleteado");
+    } else {
+        res.status(406).send("ID não encontrado!");
+    };
+});
+
+app.listen(port, () => {
+    console.log(`Servidor online!, url: http://localhost:${port}`);
+});
