015d0f7ed6ceb2c4870d0b5054fbe0f3525ef76b
Henrique Ribeiro Souto <hrshxyz@gmail.com>
2023-04-08 15:13:53 -0300

    add swagger cadastro de usuário

--- Modulo 001/Semana 10/Ex 6 - Swagger/index.js
+++ Modulo 001/Semana 10/Ex 6 - Swagger/index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocs = require("./src/swagger/swagger.json");
 
 const app = express();
 app.use(express.json());
@@ -13,8 +15,9 @@ const usersRoutes = require("./src/routes/user");
 connection.authenticate();
 connection.sync({ alter: true });
 
-app.use(tasksRoutes);
-app.use(usersRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/v1", tasksRoutes);
+app.use("/v1", usersRoutes);
 
 app.listen(port, () =>
   console.log(`Aplicação inicializada e escutando na porta ${port}`)
